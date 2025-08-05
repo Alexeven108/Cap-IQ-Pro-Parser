6e3642577d205a456a361de302187520749f6c7b
Alex Even <alexeven108@gmail.com>
2025-08-05 23:53:08 +0100

    Project Push

--- risklens/visualizer.py
+++ risklens/visualizer.py
@@ -1,2 +1,3 @@
 #here we need some line / bar / pie charts and tables
 #then we can add some subscription models to access the website for parser
+#maybe makes sense to push to full stack? 2-3 month project
