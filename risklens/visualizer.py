039b4553dfbbeeffa5a670a863de6bc5023e380b
Alex Even <alexeven108@gmail.com>
2025-08-06 23:11:49 +0100

    Project Push

--- risklens/visualizer.py
+++
@@ -1,6 +0,0 @@
-#here we need some line / bar / pie charts and tables
-#then we can add some subscription models to access the website for parser
-#maybe makes sense to push to full stack? 2-3 month project
-#going to need a jsx feature in components for frontend and pages
-#maybe a demo idea to showcase to buy side risk team
-#key metrics: liquidity risk, solvency, profitability risk, operational efficiency risk, market risk (optional add ons) and more
